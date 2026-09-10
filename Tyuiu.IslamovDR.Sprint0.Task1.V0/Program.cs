99a84df0dc98064afe0dd5f1f1f9a291d923945a
Д И <denil.2019isl1@mail.ru>
2026-09-10 20:51:58 +0500

    add Task1

---
+++ Tyuiu.IslamovDR.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.IslamovDR.Sprint0.Task1.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World");
+            string name = "g";
+            int fd = 12;
+            Console.WriteLine("что то там"+name+"продолжение чегото"+fd+"еще что то");
+            Console.ReadKey();
+        }
+    }
+}
